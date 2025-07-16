b4327128eee8fc5212a1bf5655a38dbbc94c78f3
jason-parker669 <jason6ppp@gmail.com>
2025-07-16 17:52:29 +0300

    completed task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,9 @@
+const textInput = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
 
+textInput.addEventListener("input", (event) => {
+    if (textInput.value.trim() != "") {
+        output.textContent = event.currentTarget.value.trim();
+    }
+    else output.textContent = "Anonymous";
+});
